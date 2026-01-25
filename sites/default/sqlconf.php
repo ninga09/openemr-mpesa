35374fbd15023ef5b910f94ba247d27543e11ad9
ninga09 <ningawetu@gmail.com>
2026-01-25 13:05:48 +0300

    update sqlconf.php
    The parse error in sites/default/sqlconf.php has been resolved by replacing tab characters with spaces in the variable assignments. The application should now load without syntax errors.

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -2,11 +2,11 @@
 //  OpenEMR
 //  MySQL Config
 
-$host	= 'localhost';
-$port   = '3306';
-$login	= 'openemr';
-$pass	= 'escargot';
-$dbase	= 'openemr';
+$host = 'localhost';
+$port = '3306';
+$login = 'openemr';
+$pass = 'escargot';
+$dbase = 'openemr';
 
 
 //Added ability to disable
